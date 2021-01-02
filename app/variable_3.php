0e1a692f7efae12d924249a1079ccd6c75726ae1
SOLITON\User <SolitonNew>
2021-01-02 15:48:37 +0200

    no message

--- app/variable_3.php
+++ app/variable_3.php
@@ -8,11 +8,15 @@ $varStep = $control['varStep'];
 <div class="variable-3-body">
     <div class="card">
         <div class="card-body">
-            <div class="variable-3-value">
-                <span class="variable-3-value-text text-primary" id="varValueText" ><?php print($varValue * $varStep); ?></span>
-                <span class="variable-3-value-dimension"><?php print($control['resolution']); ?></span>
+            <div class="variable-3-value-body">
+                <div class="variable-3-value-body-center">
+                    <div class="variable-3-value">
+                        <span class="variable-3-value-text text-primary" id="varValueText" ><?php print($varValue * $varStep); ?></span>
+                        <span class="variable-3-value-dimension"><?php print($control['resolution']); ?></span>
+                    </div>
+                </div>
             </div>
-            <div style="display: flex;">
+            <div class="variable-3-value-body2">
                 <div class="text-secondary"><?php print($varMin); ?> <?php print($control['resolution']); ?></div>
                 <div style="flex-grow: 1;"></div>
                 <div class="text-secondary"><?php print($varMax); ?> <?php print($control['resolution']); ?></div>
