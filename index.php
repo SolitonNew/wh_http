c984dc678b4cb90e2c2bcdd60c5a7fe4caa3efa6
SOLITON\User <SolitonNew>
2021-01-04 19:37:41 +0200

    no message

--- index.php
+++ index.php
@@ -15,7 +15,7 @@
         <link rel="shortcut icon" href="favicon.ico">
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
         <link rel="stylesheet" href="css/bootstrap.min.css">
-        <link rel="stylesheet" href="css/style.css?v=0.0.16">
+        <link rel="stylesheet" href="css/style.css?v=0.0.17">
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
         <script src="js/jquery-3.5.1.min.js"></script>
     </head>
