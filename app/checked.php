28c622edd16d2879d40397c1963079acec31472d
SOLITON\User <SolitonNew>
2021-01-07 00:09:57 +0200

    no message

--- app/checked.php
+++ app/checked.php
@@ -64,7 +64,7 @@ foreach ($rows as $row) {
     for ($i = 0; $i < count($web_color); $i++) {
         if (mb_strpos(mb_strtoupper($itemLabel), mb_strtoupper($web_color[$i]['keyword'])) !== false) {
             $color = $web_color[$i]['color'];
-            if (strpos($color, '#') !== false) {
+            if ($color) {
                 $color = "'$color'";
             }
             break;
