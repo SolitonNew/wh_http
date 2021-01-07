4186df1627596fd4e7bd9e5ad39a2cd2578734d4
SOLITON\User <SolitonNew>
2021-01-07 15:26:14 +0200

    no message

--- app/checked_edit_color.php
+++ app/checked_edit_color.php
@@ -1,12 +1,14 @@
 <div style="width: 40rem;">
-    <div class="alert alert-dark" style="display:flex;margin-bottom:1rem;">
+    <div class="alert alert-dark checked-edit-color-panel">
         <input id="keyword" type="text" class="form-control" value="" style="flex-grow:1;margin-right:1rem;">
         <input id="color" type="text" class="form-control" value="" style="width:10rem;margin-right:1rem;">
-        <a id="btn_add" href="#" class="btn btn-primary">ДОБАВИТЬ</a>
-        <a id="btn_set" href="#" class="btn btn-primary" style="margin-left:1rem;">ОБНОВИТЬ</a>
+        <div>
+            <a id="btn_add" href="#" class="btn btn-primary">ДОБАВИТЬ</a>
+            <a id="btn_set" href="#" class="btn btn-primary" style="margin-left:1rem;">ОБНОВИТЬ</a>
+        </div>
     </div>
 
-    <div class="list-group">
+    <div class="list-group checked-edit-color-list">
 <?php
         $q = $pdo->query("select VALUE from core_propertys where NAME = 'WEB_COLOR'")->fetchAll();
         if (count($q)) {
@@ -15,8 +17,10 @@
                 foreach ($a as $row) {
 ?>
         <div class="list-group-item" style="display:flex;align-items: center;">
-            <a class="set_keyword" style="flex-grow: 1;" href="#"><?php print($row['keyword']); ?></a>
-            <a class="set_color" style="width: 10rem;" href="#"><?php print($row['color']); ?></a>
+            <div>
+                <a class="set_keyword" style="flex-grow: 1;" href="#"><?php print($row['keyword']); ?></a>
+                <a class="set_color" style="width: 10rem;" href="#"><?php print($row['color']); ?></a>
+            </div>
             <a class="btn btn-primary btn-sm btn_del" href="#" data="<?php print($row['keyword']); ?>" >УДАЛИТЬ</a>
         </div>
 <?php
