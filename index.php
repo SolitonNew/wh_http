a8130f74a7b9bc56c9ca8b88d18b99f81585a35c
Moklyak Alexandr <adm@soliton.org.ua>
2025-01-06 16:36:57 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
-    header("Cache-control: public");
-    header("Expires: ".gmdate("D, d M Y H:i:s", time() + 1 * 60)." GMT");
+    header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+    header("Cache-Control: post-check=0, pre-check=0", false);
+    header("Pragma: no-cache");
 
     include 'app/connection.php';
     include 'app/utils.php';
